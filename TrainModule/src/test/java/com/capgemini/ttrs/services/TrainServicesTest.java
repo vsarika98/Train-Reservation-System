f4848ded42100fc77555425a07ad09ea143da48c
kuntal59 <68576122+kuntal59@users.noreply.github.com>
2021-12-04 15:22:36 +0530

    Add files via upload

---
+++ TrainModule/src/test/java/com/capgemini/ttrs/services/TrainServicesTest.java
@@ -0,0 +1,11 @@
+package com.capgemini.ttrs.services;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.capgemini.ttrs.repository.TrainRepository;
+
+public class TrainServicesTest {
+	
+	
+	
+}
